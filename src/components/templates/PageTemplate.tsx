4ae956622cc12c2fee6d65e2be38b5c0b3aaa56d
Arseny Bobrov <info@abobrov.com>
2020-06-25 13:45:23 +0200

    remove dummy code

--- src/components/templates/PageTemplate.tsx
+++ src/components/templates/PageTemplate.tsx
@@ -3,7 +3,6 @@ import get from 'lodash/get';
 import ComponentsRenderer from './partials/ComponentsRenderer';
 import TemplateWrapper from './partials/TemplateWrapper';
 import { Data } from '../../../prismic/types';
-import Link from '../atoms/Link/Link';
 
 interface PageTemplateProps {
   data: Data;
@@ -26,8 +25,6 @@ const PageTemplate: React.FC<PageTemplateProps> = ({
     documentRelations={documentRelations}
   >
     <main>
-      <h1>You are on: {get(data, 'data.display_name[0].text', 'unknown :((')}</h1>
-      <Link url={data.data.url} title="click me">Click me!</Link>
       <ComponentsRenderer slices={get(data, 'data.body', [])} />
     </main>
   </TemplateWrapper>
